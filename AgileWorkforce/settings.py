78698acbc66636c3f18a6eab3524be65a1814a09
Infroid <developer@infroid.in>
2019-05-27 21:17:54 +0530

    Made some changes

--- AgileWorkforce/settings.py
+++ AgileWorkforce/settings.py
@@ -39,8 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'Member.apps.MemberConfig',
-    'Sprint.apps.SprintConfig',
     'Member',
     'Sprint',
 ]
