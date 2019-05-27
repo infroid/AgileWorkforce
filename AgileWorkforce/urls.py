13879c348e51c0d9f9792200a821e767cc2ec125
Infroid <developer@infroid.in>
2019-05-27 21:58:07 +0530

    Made some changes

--- AgileWorkforce/urls.py
+++ AgileWorkforce/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from rest_framework import urls as RestFrameworkUrls
+from Member import urls as MemberUrls
 from django.urls import path
+#from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', RestFrameworkUrls),
+    path('mamber/', MemberUrls),
+    #url(r'^api-auth/', include('rest_framework.urls')),
+    #path('', include('Member.urls')),
 ]
